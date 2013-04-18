1dddd319383f7c46d46e50ba0a44374571fa03c4
Rene Reuter <support@neophoenix.net>
2013-04-18 17:14:10 +0200

    Fixing hash column type

--- sql/install.sql
+++ sql/install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `wcf1_githubdata_hash` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` int(11) NOT NULL,
-  `hash` int(11) NOT NULL,
+  `hash` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `hash` (`hash`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
